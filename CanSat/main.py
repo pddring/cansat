4f244c28be3cd9629c68efe8fbd76c476271f859
Pete Dring <dringp@fulford.york.sch.uk>
2023-12-14 14:59:20 +0000

    Switched to circuitpython

--- CanSat/main.py
+++ CanSat/main.py
@@ -1,31 +1,15 @@
-# Water quality sensor
-# Designed for use with a pico explorer board and a TDS sensor attached to ADC0
+# this uses circuitpython rather than micropython
 
-import machine
+import board
+import adafruit_bmp280
+import busio
 import time
-from breakout_bmp280 import BreakoutBMP280
-from pimoroni_i2c import PimoroniI2C
-from picographics import PicoGraphics, DISPLAY_PICO_EXPLORER, PEN_P8
 
-# set up screen
-display = PicoGraphics(display=DISPLAY_PICO_EXPLORER, pen_type=PEN_P8)
-display.set_backlight(1.0)
-WIDTH, HEIGHT = display.get_bounds()
+PINS_PICO_EXPLORER = {"sda": board.GP20, "scl": board.GP21}
 
-# set up sensors
-PINS_PICO_EXPLORER = {"sda": 20, "scl": 21}
-i2c = PimoroniI2C(**PINS_PICO_EXPLORER)
-bmp = BreakoutBMP280(i2c)
+i2c = busio.I2C(**PINS_PICO_EXPLORER)
 
-BLACK = display.create_pen(0,0,0)
-WHITE = display.create_pen(255,255,255)
-
+sensor = adafruit_bmp280.Adafruit_BMP280_I2C(i2c, address=118)
 while True:
-    display.set_pen(BLACK)
-    display.clear()
-    temperature,pressure = bmp.read()
-
-    display.set_pen(WHITE)
-    display.text("Temp: {}'C Pressure: {}".format(temperature, pressure), 0, 0, 0, 3)
-    display.update()
-    time.sleep(1.0)
+    print("Temperature: {} Pressure: {} Altitude {}".format(sensor.temperature, sensor.pressure, sensor.altitude))
+    time.sleep(1)
