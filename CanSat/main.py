e5b296c790cdc89853919240ee9e1fab594a2953
Pete Dring <dringp@fulford.york.sch.uk>
2023-12-14 16:07:41 +0000

    Working screen

--- CanSat/main.py
+++ CanSat/main.py
@@ -4,14 +4,61 @@ import board
 import adafruit_bmp280
 import busio
 import time
+import terminalio
+import displayio
+from adafruit_display_text import label
+from adafruit_st7789 import ST7789
 
 PINS_PICO_EXPLORER = {"sda": board.GP20, "scl": board.GP21}
 
 i2c = busio.I2C(**PINS_PICO_EXPLORER)
 
 sensor = adafruit_bmp280.Adafruit_BMP280_I2C(i2c, address=118)
+
+# Release any resources currently in use for the displays
+displayio.release_displays()
+
+tft_cs = board.GP17
+tft_dc = board.GP16
+spi_mosi = board.GP19
+spi_clk = board.GP18
+spi = busio.SPI(spi_clk, spi_mosi)
+try:
+    from fourwire import FourWire
+except ImportError:
+    from displayio import FourWire
+
+display_bus = FourWire(spi, command=tft_dc, chip_select=tft_cs)
+
+display = ST7789(display_bus, width=240, height=240, rowstart=80, rotation=180)
+
+"""
+# Make the display context
+splash = displayio.Group()
+display.root_group = splash
+
+color_bitmap = displayio.Bitmap(240, 240, 1)
+color_palette = displayio.Palette(1)
+color_palette[0] = 0x00FF00  # Bright Green
+
+bg_sprite = displayio.TileGrid(color_bitmap, pixel_shader=color_palette, x=0, y=0)
+splash.append(bg_sprite)
+
+# Draw a smaller inner rectangle
+inner_bitmap = displayio.Bitmap(200, 200, 1)
+inner_palette = displayio.Palette(1)
+inner_palette[0] = 0xAA0088  # Purple
+inner_sprite = displayio.TileGrid(inner_bitmap, pixel_shader=inner_palette, x=20, y=20)
+splash.append(inner_sprite)
+
+# Draw a label
+text_group = displayio.Group(scale=2, x=50, y=120)
+text = "Hello World!"
+text_area = label.Label(terminalio.FONT, text=text, color=0xFFFF00)
+text_group.append(text_area)  # Subgroup for text scaling
+splash.append(text_group)
+"""
    
-    
 # SPDX-FileCopyrightText: 2020 Bryan Siepert, written for Adafruit Industries
 #
 # SPDX-License-Identifier: Unlicense
