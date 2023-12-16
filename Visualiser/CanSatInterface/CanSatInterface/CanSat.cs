cf2cfdb9a7eea2ded8a0f6c599d13dfe511187cd
Pete Dring <dringp@fulford.york.sch.uk>
2023-12-16 09:08:26 +0000

    Added CanSatInterface

---
+++ Visualiser/CanSatInterface/CanSatInterface/CanSat.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CanSatInterface
+{
+
+    public delegate void ProcessData(string data);
+
+    public class CanSat
+    {
+        private SerialPort port;
+        private ProcessData externalHandler;
+
+        public CanSat(string PortName)
+        {
+            port = new SerialPort(PortName, 115200, Parity.None, 8);
+        }
+
+        
+        public void Connect(ProcessData OnDataReceived)
+        {
+            port.Open();
+            port.DataReceived += Port_DataReceived;
+            externalHandler = OnDataReceived;
+        }
+
+        private void Port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            switch(e.EventType)
+            {
+                case SerialData.Chars:
+                    string data = port.ReadLine();
+                    externalHandler(data);
+                    break;
+            }
+        }
+
+        public void Disconnect()
+        {
+            port.Close();
+        }
+    }
+}
---
+++ Visualiser/CanSatInterface/CanSatInterface/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.IO.Ports;
+using CanSatInterface;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // connect to the highest numbered COM port
+        Console.WriteLine("Searching for COM ports:");
+        string[] portNames = SerialPort.GetPortNames();
+        string chosenPort = "";
+        foreach (string name in portNames)
+        {
+            Console.WriteLine($"Found port: {name}");
+            chosenPort = name;
+        }
+
+        Console.WriteLine($"Attempting to connect to {chosenPort}");
+        CanSat cansat = new CanSat(chosenPort);
+        
+        // Listen for data and display it when received
+        cansat.Connect((string data) =>
+        {
+            Console.WriteLine($"Received: {data}");
+        });
+
+        // wait until the user presses enter
+        Console.ReadLine();
+        cansat.Disconnect();
+    }
+}
