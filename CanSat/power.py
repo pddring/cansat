23df38d441adf32b0dc60380ce568ebc91304503
Pete Dring <dringp@fulford.york.sch.uk>
2023-12-18 16:01:30 +0000

    Create power.py

---
+++ CanSat/power.py
@@ -0,0 +1,20 @@
+import analogio
+import digitalio
+import board
+
+# class to read from the lipo power shim
+# available from https://thepihut.com/products/lipo-shim-for-pico
+class Power:
+    def __init__(self):
+        # reads the system input voltage
+        self.vsys = analogio.AnalogIn(board.VOLTAGE_MONITOR)				
+        self.charging = digitalio.DigitalInOut(board.VBUS_SENSE)
+        
+        # reading GP24 tells us whether or not USB power is connected
+        self.charging.direction = digitalio.Direction.INPUT
+        
+    def isCharging(self):
+        return self.charging.value
+    
+    def getVoltage(self):
+        return self.vsys.value * self.vsys.reference_voltage / 65535 * 3
