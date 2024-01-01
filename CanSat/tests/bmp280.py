f971caf6bee63c0d67f4c4028d17508a6828c36f
Pete Dring <dringp@fulford.york.sch.uk>
2024-01-01 20:31:57 +0000

    Update bmp280.py
    Added calibration

--- CanSat/tests/bmp280.py
+++ CanSat/tests/bmp280.py
@@ -13,13 +13,30 @@ import adafruit_bmp280
 import busio
 import time
 
+def calibrate(pSensor, pKnownAltitude, pDebug=False):
+    while True:
+        if pDebug:
+            print("Sea level pressure: {} Altitude: {}".format(pSensor.sea_level_pressure, pSensor.altitude))
+        if pSensor.altitude > pKnownAltitude + 10:
+            pSensor.sea_level_pressure -= 1
+        elif pSensor.altitude > pKnownAltitude + 1:
+            pSensor.sea_level_pressure -= .1
+        elif pSensor.altitude < pKnownAltitude - 10:
+            pSensor.sea_level_pressure += 1
+        elif pSensor.altitude < pKnownAltitude - 1:
+            pSensor.sea_level_pressure += .1
+        else:
+            break
+    
+
 i2c = busio.I2C(sda=board.GP18, scl=board.GP19)
 
 sensor = adafruit_bmp280.Adafruit_BMP280_I2C(i2c, address=118)
 
-# change this with current weather forecast reading:
-# see https://www.metoffice.gov.uk/weather/maps-and-charts/surface-pressure
-sensor.sea_level_pressure = 988
+# change to known starting altitude
+print("Calibrating altitude")
+calibrate(sensor, 110)
+print("Sea level pressure: {} Altitude: {}".format(sensor.sea_level_pressure, sensor.altitude))
 
 while True:
     print("Temp: {:.2f} Press: {:.2f} Alt {:.2f}".format(sensor.temperature, sensor.pressure, sensor.altitude))
