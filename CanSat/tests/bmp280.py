edc02e526970ae6acd6d0da16d8205b95f69292f
Pete Dring <dringp@fulford.york.sch.uk>
2024-01-01 20:02:28 +0000

    Update bmp280.py

--- CanSat/tests/bmp280.py
+++ CanSat/tests/bmp280.py
@@ -1,25 +1,26 @@
 # Hardware: Raspberry pi pico 
 #  BMP280 pressure sensor => Pico
-# 					VIN	  => 3.3v
-#					GND	  => GND
-#				    SDA   => SDA (GP18)
-#					SCL	  => SCL (GP19)
+#                   VIN   => 3.3v
+#                   GND   => GND
+#                   SDA   => SDA (GP18)
+#                   SCL   => SCL (GP19)
 # Firmware: CircuitPython 8
 # Tests: Displays pressure, temperature and altitude on screen
-# Success: Sends values to REPL and displays them on screen
-
+# Success: Sends values to REPL 
 
 import board
 import adafruit_bmp280
 import busio
 import time
 
-
-PINS_PICO_EXPLORER = {"sda": board.GP18, "scl": board.GP19}
-
-i2c = busio.I2C(**PINS_PICO_EXPLORER)
+i2c = busio.I2C(sda=board.GP18, scl=board.GP19)
 
 sensor = adafruit_bmp280.Adafruit_BMP280_I2C(i2c, address=118)
+
+# change this with current weather forecast reading:
+# see https://www.metoffice.gov.uk/weather/maps-and-charts/surface-pressure
+sensor.sea_level_pressure = 988
+
 while True:
     print("Temp: {:.2f} Press: {:.2f} Alt {:.2f}".format(sensor.temperature, sensor.pressure, sensor.altitude))
     time.sleep(1)
