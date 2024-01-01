ca56bb9abf8461b5dbbbe2e07b128f9c9427f24f
Pete Dring <dringp@fulford.york.sch.uk>
2024-01-01 21:38:01 +0000

    Update bmp280.py
    Removed unecessary calibration

--- CanSat/tests/bmp280.py
+++ CanSat/tests/bmp280.py
@@ -13,29 +13,13 @@ import adafruit_bmp280
 import busio
 import time
 
-def calibrate(pSensor, pKnownAltitude, pDebug=False):
-    while True:
-        if pDebug:
-            print("Sea level pressure: {} Altitude: {}".format(pSensor.sea_level_pressure, pSensor.altitude))
-        if pSensor.altitude > pKnownAltitude + 10:
-            pSensor.sea_level_pressure -= 1
-        elif pSensor.altitude > pKnownAltitude + 1:
-            pSensor.sea_level_pressure -= .1
-        elif pSensor.altitude < pKnownAltitude - 10:
-            pSensor.sea_level_pressure += 1
-        elif pSensor.altitude < pKnownAltitude - 1:
-            pSensor.sea_level_pressure += .1
-        else:
-            break
-    
-
 i2c = busio.I2C(sda=board.GP18, scl=board.GP19)
 
 sensor = adafruit_bmp280.Adafruit_BMP280_I2C(i2c, address=118)
 
 # change to known starting altitude
 print("Calibrating altitude")
-calibrate(sensor, 110)
+sensor.altitude = 110
 print("Sea level pressure: {} Altitude: {}".format(sensor.sea_level_pressure, sensor.altitude))
 
 while True:
