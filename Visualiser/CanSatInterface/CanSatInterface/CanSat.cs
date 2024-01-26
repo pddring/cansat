ab3481192faee88a73d8a6dbd75ff02eaa725f43
Pete Dring <dringp@fulford.york.sch.uk>
2024-01-26 14:03:07 +0000

    Added all getters for CanSat values

--- Visualiser/CanSatInterface/CanSatInterface/CanSat.cs
+++ Visualiser/CanSatInterface/CanSatInterface/CanSat.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Transactions;
 
 namespace CanSatInterface
 {
@@ -26,6 +27,24 @@ namespace CanSatInterface
             // battery
             { "Char", 0}, // battery is charging
             { "Batt", 0}, // battery voltage (v)
+
+            // GPS
+            { "Fix", 0},  // GPS satellite fix
+            { "Sat", 0},  // GPS satellite count
+            { "3D", 0},   // 3d GPS satellite fix
+
+            // Environment
+            { "Press", 0},  // Air pressure (mb)
+            { "Temp", 0},  // Air temperature (degrees C)
+            { "Alt", 0},  // Altitude above starting level (m)
+
+            // IMU
+            { "AX", 0},  // Acceleration in x axis (m/s2)
+            { "AY", 0},  // Acceleration in y axis (m/s2)
+            { "AZ", 0},  // Acceleration in z axis (m/s2)
+            { "MX", 0},  // Magnetic field strength in x axis 
+            { "MY", 0},  // Magnetic field strength in y axis 
+            { "MZ", 0},  // Magnetic field strength in z axis 
         };
 
         public void ShowValues()
@@ -48,11 +67,117 @@ namespace CanSatInterface
         /// <summary>
         /// Gets total running time in seconds
         /// </summary>
-        /// <returns></returns>
+        /// <returns>Total running time since CanSat switched on</returns>
         public double getRunningTime()
         {
             return Values["T"];
         }
+
+        /// <summary>
+        /// Gets the latest packet number
+        /// </summary>
+        /// <returns>Latest packet number</returns>
+        public int getLatestPacketNumber()
+        {
+            return (int)Values["P"];
+        }
+
+        /// <summary>
+        /// Gets the latest battery voltage
+        /// Min: 3
+        /// Nominal: 3.7
+        /// Max: 4.2
+        /// </summary>
+        /// <returns>Battery voltage</returns>
+        public double getBatteryVoltage()
+        {
+            return Values["Batt"];
+        }
+
+        public enum GPSFixType
+        {
+            GPSFix2D,
+            GPSFix3D
+        }
+
+        /// <summary>
+        /// Checks if CanSat has a fix on GPS satellites
+        /// </summary>
+        /// <param name="fixType">2d (just lat/lng) or 3d (altitude) GPS fix</param>
+        /// <returns>true if GPS signal received from sufficient satellites</returns>
+        public bool hasGPSFix(GPSFixType fixType=GPSFixType.GPSFix2D)
+        {
+            if(fixType == GPSFixType.GPSFix3D)
+            {
+                return Values["3D"] > 0;
+            }
+            return Values["2D"] > 0;
+        }
+
+        /// <summary>
+        /// Checks if CanSat is currently plugged in to USB and is charging
+        /// </summary>
+        /// <returns>true if charging</returns>
+        public bool isCharging()
+        {
+            return Values["Char"] > 0;
+        }
+
+        /// <summary>
+        /// Gets latest pressure value
+        /// </summary>
+        /// <returns>Pressure in hPa</returns>
+        public double getPressure()
+        {
+            return Values["Press"];
+        }
+
+        /// <summary>
+        /// Gets the latest temperature value
+        /// </summary>
+        /// <returns>Temperature in 'C</returns>
+        public double getTemperature()
+        {
+            return Values["Temp"];
+        }
+        
+        /// <summary>
+        /// Gets the latest altitude value
+        /// This is a calculated value from the pressure - not an absolute value
+        /// CanSat will assume an altitude of 0 when switched on.
+        /// </summary>
+        /// <returns>Altitude (m)</returns>
+        public double getAltitude()
+        {
+            return Values["Alt"];
+        }
+
+        /// <summary>
+        /// Gets latest acceleration readings from the inertial measurement unit
+        /// </summary>
+        /// <returns>Array of X, Y an Z acceleration (m/s2)</returns>
+        public double[] getAcceleration()
+        {
+            double[] acceleration = new double[3]
+            {
+                Values["AX"], Values["AY"], Values["AZ"]
+            };
+            return acceleration;
+        }
+
+        /// <summary>
+        /// Gets latest magnetometer readings from the inertial measurement unit
+        /// </summary>
+        /// <returns>Array of X, Y an Z magnetic field strength (uT)</returns>
+        public double[] getMagneticFieldStrength()
+        {
+            double[] magneticFieldstrength = new double[3]
+            {
+                Values["MX"], Values["MY"], Values["MZ"]
+            };
+            return magneticFieldstrength;
+        }
+
 
         public CanSat(string PortName)
         {
@@ -74,7 +199,7 @@ namespace CanSatInterface
             {
                 case SerialData.Chars:
                     string data = port.ReadLine();
-                    Match m = Regex.Match(data, @"([A-Za-z]+): (-?\d+\.?\d*)");
+                    Match m = Regex.Match(data, @"([0-9A-Za-z]+): (-?\d+\.?\d*)");
                     if(m.Success)
                     {
                         string label = m.Groups[1].Value;
